6f9647edcf9178f6b8c99987cbc2321f5b402592
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2023-09-03 16:24:33 +0800

    Store data into database

--- job_application/views.py
+++ job_application/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+
 
 def index(request):
-    return  render(request, "index.html")
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        #print(form)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+            #print(first_name)
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+            messages.success(request, 'Form submitted successfully!')
+        else:
+            print('no')
+
+    return render(request, "index.html")
